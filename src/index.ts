65e4a1383d2754cb273acfd03be7c7670c5423a1
Raphael Fleury <raphael.fleury9@gmail.com>
2025-10-01 20:53:53 -0300

    feat: add openapi

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import { Elysia } from "elysia";
+import { openapi } from "@elysiajs/openapi";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .use(openapi())
+  .get("/", () => "Hello Elysia")
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
