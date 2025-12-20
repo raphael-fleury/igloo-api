196d91439f4741598164d2aaae17e04c05e13a9a
Raphael Fleury <raphael.fleury9@gmail.com>
2025-12-20 19:53:37 -0300

    fix: restructure auth middleware to use derive instead of decorate

--- src/http/middlewares/auth.middleware.ts
+++ src/http/middlewares/auth.middleware.ts
@@ -3,18 +3,19 @@ import { appDataSource } from "@/database/data-source";
 import { User } from "@/database/entities/user";
 import { Profile } from "@/database/entities/profile";
 
-const user = await appDataSource.getRepository(User)
-    .findOneBy({ id: 'b316b948-8f6c-4284-8b38-a68ca4d3dee0'}); //mock
+export const authMiddleware = async (app: Elysia) => app
+    .derive(async () => {
+        const user = await appDataSource
+            .getRepository(User)
+            .findOneBy({ id: 'b316b948-8f6c-4284-8b38-a68ca4d3dee0' }); // Mock
 
-const profile = await appDataSource.getRepository(Profile)
-    .findOneBy({ id: '14ae85e0-ec24-4c44-bfc7-1d0ba895f51d'}); //mock
+        const profile = await appDataSource
+            .getRepository(Profile)
+            .findOneBy({ id: '14ae85e0-ec24-4c44-bfc7-1d0ba895f51d' }); // Mock
 
-export const authMiddleware = (app: Elysia) => app
-    .decorate("user", user!) //mock
-    .decorate("profile", profile!) //mock
-    .guard({
-        beforeHandle({ user }) {
-            if (!user)
-                return status(401, { message: "User not authenticated" });
-        }
+        return { user: user!, profile: profile! };
+    })
+    .onBeforeHandle(({ user }) => {
+        if (!user)
+            return status(401, { message: "User not authenticated" });
     });
