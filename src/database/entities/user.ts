cf1d743b812368f216cd302dfc0d432c30534847
Raphael Fleury <raphael.fleury9@gmail.com>
2025-12-25 01:53:53 -0300

    refactor: add unique indexes for email and phone fields in user entity

--- src/database/entities/user.ts
+++ src/database/entities/user.ts
@@ -1,13 +1,15 @@
-import { Column, Entity, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, OneToMany } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, OneToMany, Index } from "typeorm";
 import { UserProfile } from "./user-profile";
 
 @Entity("users")
+@Index(["email"], { unique: true })
+@Index(["phone"], { unique: true })
 export class User {
     @PrimaryGeneratedColumn("uuid")
     id!: string;
 
     @Column({ type: "varchar", length: 15, unique: true })
-    phone?: string;
+    phone!: string;
     
     @Column({ type: "varchar", length: 100 })
     email!: string;
